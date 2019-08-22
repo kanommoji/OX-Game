f328fad71cbaec437ea58891367364de5489ef1d
kanommoji <uracha_p@kkumail.com>
2019-08-22 11:30:25 +0700

    [Created]: เพิ่มฟังก์ชัน Play NewPlayer NewGame ในไฟล์ ox.go

---
+++ ox.go
@@ -0,0 +1,46 @@
+package ox
+
+type Game struct {
+	board  [3][3]string
+	player []Player
+	turn   string
+}
+
+type Player struct {
+	name   string
+	symbol string
+	score  int
+}
+
+func (game Game) Play(player Player, row, column int) string {
+	game.marking(player, row, column)
+	winner := game.checkWin()
+	game.switchTurn()
+	return winner
+}
+
+func (game Game) marking(player Player, row, column int) {
+}
+
+func (game Game) checkWin() string {
+	return "x Winner"
+}
+
+func (game Game) switchTurn() {
+}
+
+func NewGame(player1, player2 Player, turn string) Game {
+	return Game{
+		board:  [3][3]string{},
+		player: []Player{player1, player2},
+		turn:   turn,
+	}
+}
+
+func NewPlayer(name string, symbol string) Player {
+	return Player{
+		name:   name,
+		score:  0,
+		symbol: symbol,
+	}
+}
