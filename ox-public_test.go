019881c6981bfa1a5fe50b706beaa8ed52f27d04
kanommoji <uracha_p@kkumail.com>
2019-08-22 14:49:37 +0700

    [Added]: เพิ่มเทส Test_X_Win_Vertical_SecondLine_Line_Should_Be_X_Winner ในไฟล์ ox-public_test.go

--- ox-public_test.go
+++ ox-public_test.go
@@ -101,3 +101,22 @@ func Test_O_Win_Vertical_First_Line_Should_Be_O_Winner(t *testing.T) {
 		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
+
+func Test_X_Win_Vertical_SecondLine_Line_Should_Be_X_Winner(t *testing.T) {
+	expected := "x Winner"
+	player1 := ox.NewPlayer("mo", "x")
+	player2 := ox.NewPlayer("praw", "o")
+	game := ox.NewGame(player1, player2, "x")
+
+	game.Play(player1, 1, 1)
+	game.Play(player2, 0, 0)
+	game.Play(player1, 2, 1)
+	game.Play(player2, 1, 0)
+	winner := game.Play(player1, 0, 1)
+
+	actual := winner
+
+	if expected != actual {
+		t.Errorf("Expect %s but got %s", expected, actual)
+	}
+}
