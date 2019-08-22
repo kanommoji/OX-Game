9326f59f30ea7c7845254af67e3867f3f3263084
kanommoji <uracha_p@kkumail.com>
2019-08-22 15:53:15 +0700

    [Added]: เพิ่มเทส Test_Mark_Full_Board_And_X_Win_Horizon_Shoud_Be_X_Winner ในไฟล์ ox-public_test.go

--- ox-public_test.go
+++ ox-public_test.go
@@ -191,3 +191,25 @@ func Test_Mark_Full_Board_Shoud_Be_Tie(t *testing.T) {
 		t.Errorf("Expect %v but got %v", expected, actual)
 	}
 }
+
+func Test_Mark_Full_Board_And_X_Win_Horizon_Shoud_Be_X_Winner(t *testing.T) {
+	expected := "x Winner"
+	player1 := ox.NewPlayer("mo", "x")
+	player2 := ox.NewPlayer("praw", "o")
+	game := ox.NewGame(player1, player2, "x")
+
+	game.Play(player1, 0, 0)
+	game.Play(player2, 1, 0)
+	game.Play(player1, 2, 1)
+	game.Play(player2, 1, 1)
+	game.Play(player1, 1, 2)
+	game.Play(player2, 2, 2)
+	game.Play(player1, 0, 1)
+	game.Play(player2, 2, 0)
+	winner := game.Play(player1, 0, 2)
+	actual := winner
+
+	if expected != actual {
+		t.Errorf("Expect %v but got %v", expected, actual)
+	}
+}
