af60559594eaaa97d8b8bb17a3a903d159009b27
kanommoji <uracha_p@kkumail.com>
2019-08-23 00:37:35 +0700

    [Added]: เพิ่มฟังก์ชัน checkBoardPositionEmpty และเรียกใช้งานใน marking ในไฟล์ ox.go

--- ox.go
+++ ox.go
@@ -13,14 +13,24 @@ type Player struct {
 }
 
 func (game *Game) Play(row, column int) string {
-	game.marking(row, column)
+	statusMarking := game.marking(row, column)
 	winner := game.checkWin()
-	game.switchTurn()
+	if statusMarking {
+		game.switchTurn()
+	}
 	return winner
 }
 
-func (game *Game) marking(row, column int) {
+func (game Game) checkBoardPositionEmpty(row, column int) bool {
+	return game.Board[row][column] == ""
+}
+
+func (game *Game) marking(row, column int) bool {
+	if !game.checkBoardPositionEmpty(row, column) {
+		return false
+	}
 	game.Board[row][column] = game.turn
+	return true
 }
 
 func (game Game) checkWin() string {
