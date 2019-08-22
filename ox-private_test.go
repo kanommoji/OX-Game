8c165a9f2450b9e79a12c1912c0f60e43b525d95
kanommoji <uracha_p@kkumail.com>
2019-08-22 15:14:22 +0700

    [Added]: เพิ่มเทส Test_CheckFullBoard_Input_Mark_X_And_O_Full_Board_Should_Be_True ในไฟล์ ox-private_test.go

--- ox-private_test.go
+++ ox-private_test.go
@@ -110,3 +110,25 @@ func Test_CheckWin_Input_O_Mark_Horizon_Third_Line_Should_Be_O_Winner(t *testing
 		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
+
+func Test_CheckFullBoard_Input_Mark_X_And_O_Full_Board_Should_Be_True(t *testing.T) {
+	expected := true
+	player1 := NewPlayer("mo", "x")
+	player2 := NewPlayer("praw", "o")
+	game := NewGame(player1, player2, "x")
+
+	game.marking(player1, 0, 0)
+	game.marking(player2, 0, 1)
+	game.marking(player1, 0, 2)
+	game.marking(player2, 1, 0)
+	game.marking(player1, 1, 1)
+	game.marking(player2, 1, 2)
+	game.marking(player1, 2, 0)
+	game.marking(player2, 2, 1)
+	game.marking(player1, 2, 2)
+	actual := game.checkFullBoard()
+
+	if expected != actual {
+		t.Errorf("Expect %v but got %v", expected, actual)
+	}
+}
