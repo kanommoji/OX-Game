740fdc96380590ef68d44f015e8739a55e421c1b
kanommoji <uracha_p@kkumail.com>
2019-08-22 14:31:58 +0700

    [Edited]: แก้ไขเทส Test_O_Win_Horizon_Third_Line_Should_Be_O_Winner ในไฟล์ ox-public_test.go

--- ox-public_test.go
+++ ox-public_test.go
@@ -61,3 +61,23 @@ func Test_X_Win_Horizon_Third_Line_Should_Be_X_Winner(t *testing.T) {
 		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
+
+func Test_O_Win_Horizon_Third_Line_Should_Be_O_Winner(t *testing.T) {
+	expected := "o Winner"
+	player1 := ox.NewPlayer("mo", "x")
+	player2 := ox.NewPlayer("praw", "o")
+	game := ox.NewGame(player1, player2, "x")
+
+	game.Play(player1, 1, 0)
+	game.Play(player2, 2, 1)
+	game.Play(player1, 1, 1)
+	game.Play(player2, 2, 0)
+	game.Play(player1, 0, 1)
+	winner := game.Play(player2, 2, 2)
+
+	actual := winner
+
+	if expected != actual {
+		t.Errorf("Expect %s but got %s", expected, actual)
+	}
+}
