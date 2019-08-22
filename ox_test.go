089b5554b361bb29de799aab44926ab36ad33704
kanommoji <uracha_p@kkumail.com>
2019-08-22 12:03:25 +0700

    [Edited]: แก้ไขรูปแบบโค้ด ในไฟล์ ox_test.go

--- ox_test.go
+++ ox_test.go
@@ -15,11 +15,12 @@ func Test_Play_Input_X_Mark_Horizon_First_Line_Should_Be_X_Winner(t *testing.T) 
 	game.Play(player2, 1, 1)
 	game.Play(player1, 0, 1)
 	game.Play(player2, 1, 0)
+	winner := game.Play(player1, 0, 2)
 
-	actual := game.Play(player1, 0, 2)
+	actual := winner
 
 	if expected != actual {
-		t.Errorf("Expect %s bot got %s", expected, actual)
+		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 
 }
@@ -36,7 +37,7 @@ func Test_Marking_Input_X_Row_0_Column_0_Should_Be_Row_0_Column_0_Is_X(t *testin
 	actual := game.Board[0][0]
 
 	if expected != actual {
-		t.Errorf("Expect %s bot got %s", expected, actual)
+		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
 
@@ -52,7 +53,7 @@ func Test_Marking_Input_O_Row_1_Column_0_Should_Be_Row_1_Column_0_Is_O(t *testin
 	actual := game.Board[1][0]
 
 	if expected != actual {
-		t.Errorf("Expect %s bot got %s", expected, actual)
+		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
 
@@ -66,7 +67,7 @@ func Test_SwitchTurn_Input_x_Play_Should_Be_Turn_o(t *testing.T) {
 	actual := game.turn
 
 	if expected != actual {
-		t.Errorf("Expect %s bot got %s", expected, actual)
+		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
 
@@ -80,7 +81,7 @@ func Test_SwitchTurn_Input_o_Play_Should_Be_Turn_x(t *testing.T) {
 	actual := game.turn
 
 	if expected != actual {
-		t.Errorf("Expect %s bot got %s", expected, actual)
+		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
 
@@ -92,11 +93,25 @@ func Test_CheckWin_Input_X_Mark_Horizon_First_Line_Should_Be_X_Winner(t *testing
 
 	game.marking(player1, 0, 0)
 	game.marking(player1, 0, 1)
-	game.marking(player1, 2, 2)
+	game.marking(player1, 0, 2)
 
 	actual := game.checkWin()
 
 	if expected != actual {
-		t.Errorf("Expect %s bot got %s", expected, actual)
+		t.Errorf("Expect %s but got %s", expected, actual)
+	}
+}
+
+func Test_CheckWin_Input_X_Mark_Row_0_Column_0_Should_Be_Next_Turn(t *testing.T) {
+	expected := "Next Turn"
+	player1 := NewPlayer("mo", "x")
+	player2 := NewPlayer("praw", "o")
+	game := NewGame(player1, player2, "x")
+
+	game.marking(player1, 0, 0)
+	actual := game.checkWin()
+
+	if expected != actual {
+		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
