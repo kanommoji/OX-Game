0e5bcc6a95401ff827d85bd072c0586a72f97974
kanommoji <uracha_p@kkumail.com>
2019-08-22 14:08:19 +0700

    [Added]: เพิ่มเทส Test_X_Win_Horizon_Second_Line_Should_Be_X_Winner ในไฟล์ ox-public_test.go

--- ox-public_test.go
+++ ox-public_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_Play_Input_X_Mark_Horizon_First_Line_Should_Be_X_Winner(t *testing.T) {
+func Test_X_Win_By_Horizon_First_Line_Should_Be_X_Winner(t *testing.T) {
 	expected := "x Winner"
 	player1 := ox.NewPlayer("mo", "x")
 	player2 := ox.NewPlayer("praw", "o")
@@ -16,6 +16,25 @@ func Test_Play_Input_X_Mark_Horizon_First_Line_Should_Be_X_Winner(t *testing.T) 
 	game.Play(player1, 0, 1)
 	game.Play(player2, 1, 0)
 	winner := game.Play(player1, 0, 2)
+
+	actual := winner
+
+	if expected != actual {
+		t.Errorf("Expect %s but got %s", expected, actual)
+	}
+}
+
+func Test_X_Win_Horizon_Second_Line_Should_Be_X_Winner(t *testing.T) {
+	expected := "x Winner"
+	player1 := ox.NewPlayer("mo", "x")
+	player2 := ox.NewPlayer("praw", "o")
+	game := ox.NewGame(player1, player2, "x")
+
+	game.Play(player1, 1, 0)
+	game.Play(player2, 0, 1)
+	game.Play(player1, 1, 1)
+	game.Play(player2, 0, 0)
+	winner := game.Play(player1, 1, 2)
 
 	actual := winner
 
