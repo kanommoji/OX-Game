ae858d81f50e822140858434a60f0bc89b40fbe1
kanommoji <uracha_p@kkumail.com>
2019-08-22 15:01:41 +0700

    [Added]: เพิ่มเทส Test_X_Win_Left_Diagonal_Line_Should_Be_X_Winner ในไฟล์ ox-public_test.go

--- ox-public_test.go
+++ ox-public_test.go
@@ -151,3 +151,21 @@ func Test_O_Win_Right_Diagonal_Line_Should_Be_O_Winner(t *testing.T) {
 		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
+
+func Test_X_Win_Right_Diagonal_Line_Should_Be_X_Winner(t *testing.T) {
+	expected := "x Winner"
+	player1 := ox.NewPlayer("mo", "x")
+	player2 := ox.NewPlayer("praw", "o")
+	game := ox.NewGame(player1, player2, "x")
+
+	game.Play(player1, 2, 0)
+	game.Play(player2, 0, 0)
+	game.Play(player1, 1, 1)
+	game.Play(player2, 2, 1)
+	winner := game.Play(player1, 0, 2)
+	actual := winner
+
+	if expected != actual {
+		t.Errorf("Expect %s but got %s", expected, actual)
+	}
+}
