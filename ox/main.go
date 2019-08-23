3a815d64b98ded71c6424192a3f9931e031c9411
kanommoji <uracha_p@kkumail.com>
2019-08-23 14:32:59 +0700

    [Created]: สร้าง main.go

--- ox/main.go
+++ ox/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"ox/api"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	var gameApi api.GameAPI
+	route := gin.Default()
+	route.POST("ox/new-game/player", gameApi.AddPlayer)
+	route.POST("ox/new-game", gameApi.SetNewGame)
+	route.Run(":8080")
 }
